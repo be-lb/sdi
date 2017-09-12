2e65db2d05da0a2b2a90429e99239fd077f84fdf
Pierre Marchand <pierremarc07@gmail.com>
2017-09-12 13:30:46 +0200

    added license headers

--- documents/views.py
+++ documents/views.py
@@ -1,4 +1,19 @@
-# from django.shortcuts import render
+#########################################################################
+#  Copyright (C) 2017 Atelier Cartographique <contact@atelier-cartographique.be>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3 of the License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#########################################################################
+
 from django.http import HttpResponseBadRequest, JsonResponse, FileResponse
 from django.urls import reverse
 from django import forms
