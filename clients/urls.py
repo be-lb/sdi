31fc62bac4cd93d623c1f73c6562fd6535951762
Pierre Marchand <pierre-m@atelier-cartographique.be>
2017-09-25 07:58:52 +0200

    fixed client urls

--- clients/urls.py
+++ clients/urls.py
@@ -19,11 +19,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^bundle/(?P<app_name>.+)/(?P<path>.*)$',
+    url(r'^bundle/(?P<app_name>[-_\w]+)/(?P<path>.*)$',
         views.app, name='clients.bundle'),
-    url(r'^assets/(?P<app_name>.+)/(?P<path>.*)$',
+    url(r'^assets/(?P<app_name>[-_\w]+)/(?P<path>.*)$',
         views.style, name='clients.assets'),
-    url(r'^(?P<app_name>.+)/(?P<path>.*)$',
+    url(r'^(?P<app_name>[-_\w]+)/(?P<path>.*)$',
         views.app_index, name='clients.root'),
     url(r'^$', views.index, name='clients.index'),
 ]
