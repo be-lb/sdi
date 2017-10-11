eecd96880080a4c3aae68cf81f423b68d3553237
Pierre Marchand <pierremarc07@gmail.com>
2017-10-11 16:59:18 +0200

    help with bundles sourcemaps

--- clients/templates/clients/bundle.js
+++ clients/templates/clients/bundle.js
@@ -1,12 +1,6 @@
+(function () { var SDI = Object.create({ {% if user_id %} user: "{{ user_id }}", {% else %} user: null, {% endif %} args: "{{ path }}".split('/'), api : "{{ api }}", csrf : "{{ csrf_token }}", }); var bundle = function defaultMain() { console.error('Bundle not loaded'); }; {% autoescape off %} {{ bundle }} {% endautoescape %}
+/* Joined up to here to make mappings correctly point to line numbers */
 
-(function () {
-    var SDI = Object.create({ {% if user_id %} user: "{{ user_id }}", {% else %} user: null, {% endif %} args: "{{ path }}".split('/'), api : "{{ api }}", csrf : "{{ csrf_token }}", });
-    var bundle = function defaultMain() {
-                console.error('Bundle not loaded');
-            };
-    {% autoescape off %}
-    {{ bundle }}
-    {% endautoescape %}
 
 document.onreadystatechange = function startApplication() {
     if ('interactive' === document.readyState) {
