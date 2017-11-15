142ea9b310bd84d331eebad51d8f030647afe050
Pierre Marchand <pierremarc07@gmail.com>
2017-11-15 11:54:31 +0100

    api alias create and update

--- api/serializers/alias.py
+++ api/serializers/alias.py
@@ -17,6 +17,7 @@
 from rest_framework import serializers
 
 from ..models import Alias
+from ..models.message import message
 from .message import MessageRecordSerializer
 
 
@@ -27,3 +28,25 @@ class AliasSerializer(serializers.ModelSerializer):
     class Meta:
         model = Alias
         fields = ('id', 'select', 'replace')
+
+
+    def create(self, validated_data):
+        select_data = validated_data.pop('select')
+        replace_data = validated_data.pop('replace')
+
+        replace = message(**replace_data)
+        
+        return Alias.objects.create(
+            select=select_data,
+            replace=replace
+        )
+
+    def update(self, i, validated_data):
+        select_data = validated_data.pop('select')
+        replace_data = validated_data.pop('replace')
+        print('alias update {}'.format(type(replace_data)))
+        i.replace.update_record(**replace_data)
+        i.select = select_data
+
+        return i
+
