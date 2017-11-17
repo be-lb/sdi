ba7387b17d86d8571a39d4eb23924bbf4366836c
Pierre Marchand <pierre-m@atelier-cartographique.be>
2017-11-17 16:14:51 +0100

    timeserie per name

--- timeserie/views.py
+++ timeserie/views.py
@@ -1,22 +1,19 @@
-
 import json
 
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.db import connections
 
 from .models import TimeserieRecord
 
 
-
 def timeserie(request, id):
 
-    record = get_object_or_404(TimeserieRecord, pk=id)
+    record = get_object_or_404(TimeserieRecord, name=id)
     model = record.create_model()
-    
+
     data = []
     for t in model.objects.all():
         data.append(t.make_record())
 
-    
     return JsonResponse(data, safe=False)
